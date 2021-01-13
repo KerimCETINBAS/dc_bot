332400cd4c919d480ac08f68d45d465048babfd2
KerimCetinbas <a.kerim.cetinbasyandex.com>
2021-01-13 07:31:09 +0300

    crash fixed on router

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('keppAlive'));
 
 
 mongoose.connect(`${process.env.DB_URI}`,{useNewUrlParser: true, useFindAndModify : false, useCreateIndex : true, useUnifiedTopology: true })
--- src/router.js
+++ src/router.js
@@ -1,28 +1,42 @@
-import { exists, existsSync } from 'fs'
+import { existsSync } from 'fs'
 import { config } from 'dotenv'
 config()
 
 const prefix = process.env.BOT_PREFIX
 
+
 export default async msg => {
- 
 
     try {
-        
         let args = msg.content.split(prefix)[1]
         args = args.trim().replace(/[\s]+/g , " ").toLowerCase().split(" ")
-        
+        /**
+         *  @msg {discord message collection}
+         *  @controller describes controller class imported
+         *  @action  describes controller class function
+         *  @args arguments
+         **/
         const controller = args.shift()
         const action = args.shift()
+        /** if controller file exist */
         if (existsSync(`${__dirname}/controllers/${controller}.controller.js`)) {
+            // import & call controller
             let Router = await import(`${__dirname}/controllers/${controller}.controller.js`)
             Router = Router.default
             await Router.Router(msg, controller, action, args)
          
-        } else msg.reply(`Tanınmayan komut : "${controller}" , Komutu yanlış girdiniz veya sistem tarafından tanınmıyor`)
+        } else /* Else return error msg */ {
         
+            const reply = await msg.reply(`Tanınmayan komut : "${controller}" , Komutu yanlış girdiniz veya sistem tarafından tanınmıyor`)
 
-    } catch (error) { console.error(error) }
+            setTimeout( () => {
+                reply.delete()
+                msg.delete()
+            },2000)
+            
+        }
+
+    } catch (e) { console.error.bind(e) }
 
 
 }
